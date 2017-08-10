bef45edf075aa04349eee993c2e04e1c8e76bd45
Tony Baulos <baulost@gmail.com>
2017-08-10 13:25:21 -0400

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Tony Baulos on 8/10/17.
+//  Copyright © 2017 LearnSwiftAugustaGA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
