5e42491149f9243528db37e720db648dd67a90fc
Tony Baulos <baulost@gmail.com>
2017-08-10 14:08:39 -0400

    Guess which flag: Random numbers

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 LearnSwiftAugustaGA. All rights reserved.
 //
 
+import GameplayKit
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     var countries = [String]()
+    var correctAnswer = 0
     var score = 0
 
     
@@ -25,7 +27,7 @@ class ViewController: UIViewController {
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
         button3.layer.borderWidth = 1
-        
+
         button1.layer.borderColor = UIColor.lightGray.cgColor
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
@@ -34,9 +36,13 @@ class ViewController: UIViewController {
     }
 
     func askQuestion() {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+        title = countries[correctAnswer].uppercased()
+
     }
 
     override func didReceiveMemoryWarning() {
