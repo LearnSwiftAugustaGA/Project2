c9e25fa59c4fe5ec06d5af10585d2011e14b91d7
Tony Baulos <baulost@gmail.com>
2017-08-10 14:18:26 -0400

    From outlets to actions: IBAction and string interpolation.    This completes Project2.

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -32,17 +32,32 @@ class ViewController: UIViewController {
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
 
-        askQuestion()
+        askQuestion(action: nil)
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction!) {
         countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
         title = countries[correctAnswer].uppercased()
+    }
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
